d9b0bcf0f02695352a5792c32d2d7c0a90f29368
isen <mustbeisen@gmail.com>
2017-06-27 09:05:14 +0300

    Homework 24.06.2017

---
+++ HomeWork240617/Main.java
@@ -0,0 +1,43 @@
+package HomeWork240617;
+
+import org.omg.Messaging.SYNC_WITH_TRANSPORT;
+
+import static HomeWork240617.SimpleUnit.*;
+
+/**
+
+ * Created by Ilya on 26.06.2017.
+ */
+public class Main {
+
+    public static void main(String[] args){
+
+        //sing(int a) test
+        //
+        System.out.println(sing(-24));
+        System.out.println(sing(0));
+        System.out.println(sing(13));
+
+
+        //mod(int a,int b) test
+        //
+        System.out.println(mod(20,2));
+        System.out.println(mod(55,0));
+        System.out.println(mod(-12,5));
+
+
+        //isPrime(int a) test
+        //
+        System.out.println(isPrime(123));
+        System.out.println(isPrime(2));
+        System.out.println(isPrime(13));
+
+
+        //isFibonacci(int a) test
+        //
+        System.out.println(isFibonacci(14));
+        System.out.println(isFibonacci(1));
+        System.out.println(isFibonacci(55));
+
+    }
+}
